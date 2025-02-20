d72d91e3f449003b98c3612ce747eb79fe5ddbd4
xzzaaa <133853510+xzzaaa@users.noreply.github.com>
2025-02-20 14:34:12 +0100

    second commit

---
+++ public_html/header.php
@@ -0,0 +1,6 @@
+<div class="header-container">
+    <a href="index.php">Home</a>
+    <a href="doomscrollblog.php">pastblogs</a>
+    <a href="index.php">Home</a>
+    <a href="index.php">Home</a>
+</div>

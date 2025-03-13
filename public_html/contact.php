96595b27c17e64f9d8f451ef93341df7ea7ca49d
bicur <telly.lange@gmail.com>
2025-03-13 13:41:47 +0100

    just a little contact shirr 13/03

--- public_html/contact.php
+++ public_html/contact.php
@@ -1,5 +1,48 @@
 <?php
+
+
+use PHPMailer\PHPMailer\PHPMailer;
+use PHPMailer\PHPMailer\SMTP;
+use PHPMailer\PHPMailer\Exception;
+
+require '../vendor/autoload.php'; // Include Composer's autoloader
+
 include("header.php");
+
+if ($_SERVER["REQUEST_METHOD"] == "POST") {
+    // Hämta formulärdata
+    $fname = htmlspecialchars($_POST['fname']);
+    $lname = htmlspecialchars($_POST['lname']);
+    $email = htmlspecialchars($_POST['email']);
+    $message = htmlspecialchars($_POST['message']);
+
+    $mail = new PHPMailer(true);
+    // Skapa e-postmeddelandet
+    try {
+        // Server settings
+        $mail->isSMTP();
+        $mail->Host = 'MS_xKGp2r@trial-k68zxl2j5r94j905.mlsender.net'; // SMTP-server (t.ex. Gmail)
+        $mail->SMTPAuth = true;
+        $mail->Username = 'lbsuppgift@gmail.com'; // Din e-postadress
+        $mail->Password = 'WebbGrej0+'; // Ditt e-postlösenord
+        $mail->SMTPSecure = PHPMailer::ENCRYPTION_STARTTLS; // Kryptering
+        $mail->Port = 587; // Port för SMTP
+
+        // Mottagare
+        $mail->setFrom($email, "$fname $lname");
+        $mail->addAddress('telly.lange@elev.ga.lbs.se'); // Ersätt med mottagarens e-post
+
+        // Innehåll
+        $mail->Subject = 'Kontaktmeddelande';
+        $mail->Body = "Förnamn: $fname\nEfternamn: $lname\nE-post: $email\n\nMeddelande:\n$message";
+
+        // Skicka e-post
+        $mail->send();
+        echo "<p>Meddelandet har skickats!</p>";
+    } catch (Exception $e) {
+        echo "<p>Något gick fel. Försök igen senare. Felmeddelande: {$mail->ErrorInfo}</p>";
+    }
+}
 ?>
 
 <!DOCTYPE html>
@@ -20,14 +63,12 @@ include("header.php");
     </div>
     <div id="contactHeader">
         <h1>Contact Us:</h1>
-    </div>
+    </div>  
     <div class="contactForm">
-        <form method="post" action="mailto:telly.lange@elev.ga.lbs.se">
+        <form method="post" action="">
             <div class="contactContent">
-                <label for="fname">First name:</label><br>
-                <input type="text" id="fname" name="fname" required><br>
-                <label for="lname">Last name:</label><br>
-                <input type="text" id="lname" name="lname" required><br>
+                <label for="name">Full name:</label><br>
+                <input type="text" id="name" name="name" required><br>
                 <label for="email">Enter your email:</label><br>
                 <input type="email" id="email" name="email" required><br>
                 <label for="message">Message:</label><br>
